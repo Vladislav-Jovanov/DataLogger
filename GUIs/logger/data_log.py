593b2596603692f2f7f9960dcaefdfa168b16738
Vladislav Jovanov <vladislav.jovanov@ihtm.bg.ac.rs>
2025-10-23 22:06:11 +0200

    submodule update and app overhaul

--- GUIs/logger/data_log.py
+++ GUIs/logger/data_log.py
@@ -5,13 +5,17 @@ Created on Wed Jan 17 23:05:06 2024
 
 @author: tze
 """
-from tkWindget.tkWindget import AppFrame, OnOffButton, FigureFrame, IPEntry, LabelFrame
+from tkWindget.tkWindget import AppFrame, OnOffButton, FigureFrame, IPEntry, LabelFrame, SaveSingleFile
+from RW_data.RW_files import Read_from, Write_to
 from RW_data.RW_files import Files_RW
 from tkinter import Frame, Button, Label, StringVar, IntVar, DoubleVar, OptionMenu, _setit, DISABLED, NORMAL
 import os
 import time
 from tkinter.filedialog import asksaveasfilename
-import numpy as np
+from numpy import newaxis, linspace, append, shape, array
+from numpy import min as npmin
+from numpy import max as npmax
+from Figures.Figures import FigureXY2
 from Figure.Figure import FigureE60
 import socket
 
@@ -23,34 +27,21 @@ class Logger(AppFrame):
         super().__init__(**kwargs,file=__file__,appgeometry=(900,600,10,10))
         self.init_variables()
         try:
-            tmp=Files_RW().check_IV_measure_ini(self.scriptdir,self.ini_name,Files_RW.split)
-            self.savedir=tmp.savedir
-        except:
-            self.savedir='Documents'
-            self.write_to_ini()
-        try:
-            filename=self.ini_name.split('.')[0]+'.instr'
-            tmp=Files_RW().check_IV_measure_inst_file(self.scriptdir,filename,':=')    
-            self.ipaddr=tmp[0][0]
-            self.sockport=tmp[1][0]
+            self.devices=Read_from.ini_inst(self.file,extension='instr')
+            self.ipaddr=self.devices["#device_1"]['ip_address']
+            self.sockport=self.devices["#device_1"]['port']
         except:
             self.ipaddr='192.168.1.210'
             self.sockport=5025
         self.init_frames()
         self.init_commandframe()
-        
-    def write_to_ini(self):
-        write=[]
-        write.append(f'save_file_path{Files_RW.split}{self.savedir}')
-        Files_RW().write_to_file(self.scriptdir,self.ini_name,write)
-    
+
     def write_to_instr(self):
         write=[]
         write.append(f'ip_address{Files_RW.split}{self.command_elements["ip"].get_address()}')
         write.append(f'port{Files_RW.split}{self.command_elements["ip"].get_port()}')
         Files_RW().write_to_file(self.scriptdir,self.ini_name.split('.')[0]+'.instr',write)
-        
-        
+
     def init_variables(self):
         self.scriptdir=os.path.dirname(__file__)
         self.ini_name=os.path.basename(__file__).replace(os.path.basename(__file__).split('.')[-1],'ini')
@@ -72,28 +63,24 @@ class Logger(AppFrame):
         self.measurement_init=False
         self.command_list={'connect':{'on':self.connect,'off':self.disconnect},'collect':{'on':self.collect_plot,'off':self.stop_collect_plot}}
         self.command_elements={}
-        
+
     def update_time_between(self):
         if self.command_elements["zrck"].get_state()=="on":    
             self.time_between_points=self.takt*(self.variables['integration'].get()*2+self.variables["delay"].get()) #seconds
         else:
             self.time_between_points=self.takt*(self.variables['integration'].get()+self.variables["delay"].get())
-        self.sample_time=np.linspace(-(self.variables['samples'].get()-1)*self.time_between_points,0,self.variables['samples'].get())
-        
-        
+        self.sample_time=linspace(-(self.variables['samples'].get()-1)*self.time_between_points,0,self.variables['samples'].get())
+
     def init_frames(self):
         self.commandframe = Frame(self.frameroot)
         self.commandframe.grid(column=0,row=0)
         self.commandframe.columnconfigure(0, weight = 1)
         self.commandframe.rowconfigure(0, weight = 1)
-        self.figure=FigureFrame(parent=self.frameroot,figclass=FigureE60)
-        self.figure.grid(row=0,column=1)
-        self.figure.columnconfigure(0, weight = 1)
-        self.figure.rowconfigure(0, weight = 1)
-        self.figure.plot.ax.set_xlim(0,1)
-        self.figure.plot.ax.set_xlabel(f'{self.xname} ({self.units[self.xname]})',fontsize=10, position=(0.5,0),labelpad=5)
-        self.figure.plot.ax.locator_params(axis='x', nbins=4)
-        
+        self.figure=FigureFrame(parent=self.frameroot,figclass=FigureXY2,figkwargs={'figsize':(15/2.54,8/2.54),'axsize':[2/15,3/15,7/15,5/8]})
+        self.figure.grid(column=1,row=0)
+        self.figure.plot.update_labels(x=f'{self.xname} ({self.units[self.xname]})')
+        self.figure.plot.set_x_grid_lines(4)
+
     def init_commandframe(self):
         rowcount=1
         tmpframe=Frame(self.commandframe)
@@ -102,15 +89,15 @@ class Logger(AppFrame):
         tmp.grid(row=1,column=1)
         self.command_elements['connect']=tmp
         self.command_elements['connect'].enable_press()
-        
+
         self.command_elements['ip']=IPEntry(parent=tmpframe,address=f"{self.ipaddr}:{self.sockport}")
         #self.command_elements['ip']=IPEntry(parent=self.commandframe)
         self.command_elements['ip'].grid(row=rowcount,column=2)
-        
+
         rowcount+=1;
         self.instname=LabelFrame(parent=self.commandframe,width=32,height=2)
         self.instname.grid(row=rowcount,column=1)
-        
+
         rowcount+=1
         tmpframe=Frame(self.commandframe)
         tmpframe.grid(row=rowcount,column=1)
@@ -125,7 +112,7 @@ class Logger(AppFrame):
         self.command_elements['type'].grid(row=2,column=3,sticky="W")
         self.command_elements['range']=OptionMenu(tmpframe,self.variables["range"],*self.quantities_details[self.quantities[0]][self.types[0]])
         self.command_elements['range'].grid(row=2,column=2,sticky="W")
-        
+
         rowcount+=1
         tmpframe=Frame(self.commandframe)
         tmpframe.grid(row=rowcount,column=1)
@@ -142,7 +129,7 @@ class Logger(AppFrame):
         self.command_elements['zrck'].grid(row=2,column=2)
         self.command_elements['samples']=OptionMenu(tmpframe,self.variables['samples'],*[1,20,50,100,1000,5000])
         self.command_elements['samples'].grid(row=2,column=3)
-        
+
         rowcount+=1
         tmpframe=Frame(self.commandframe)
         tmpframe.grid(row=rowcount,column=1)
@@ -151,11 +138,13 @@ class Logger(AppFrame):
         self.command_elements['collect']=tmp
         Button(tmpframe,text="Clear plot",command=self.clear_plot_data).grid(row=1,column=2)
         rowcount+=1
-        self.command_elements['save']=Button(self.commandframe, text="Save Data", width=12, command=self.save_data)
+        self.command_elements['save']=SaveSingleFile(parent=self.commandframe,ini=self.ini, write_ini=self.write_ini, text='Save Data', filetypes=[(("log data file","*.log") )],write=self.placeholder)
+        self.command_elements['save'].add_filename('Time_log')
+        #self.command_elements['save']=Button(self.commandframe, text="Save Data", width=12, command=self.save_data)
         self.command_elements['save'].grid(row=rowcount,column=0,columnspan=2)
-        self.command_elements['save'].configure(state="disabled")
+        #self.command_elements['save'].config(state="disabled")
         self.set_defaults()
-    
+
     def set_defaults(self):
         self.variables['quantity'].set(self.quantities[0])
         self.variables['type'].set(self.types[0])
@@ -165,14 +154,12 @@ class Logger(AppFrame):
         self.variables['delay'].set(1)
         self.variables['samples'].set(100)
         self.disable_settings_elements()
-        
-    
+
     def update_range(self,var):
         self.change_options(self.command_elements["range"], self.variables["range"], self.quantities_details[self.variables["quantity"].get()][var])
         if self.variables["range"].get() not in [str(item) for item in self.quantities_details[self.variables["quantity"].get()][var]]:
             self.variables["range"].set(self.quantities_details[self.variables["quantity"].get()][var][-1])
-        
-    
+
     def update_quantity(self,var):
         self.variables["unit"].set(self.units[var])
         self.change_options(self.command_elements["range"], self.variables["range"], self.quantities_details[var][self.variables["type"].get()])
@@ -187,32 +174,37 @@ class Logger(AppFrame):
     
     def press_test(self,item):
         self.command_list[item][self.command_elements[item].get_state()]()
-    
+
     def placeholder(self):
         pass
-    
-    def save_data(self):
-        header=[]
-        text='#comment'
-        header.append(text)
-        text="#setup"
-        header.append(text)
-        text='#data_header'
-        header.append(text)
-        text=f'{self.xname}\t{self.variables["quantity"].get()}'
-        header.append(text)
-        text=f'{self.units[self.xname]}\t{self.units[self.variables["quantity"].get()]}'
-        header.append(text)
-        text='#data_table'
-        header.append(text)
-        data=np.append(self.datatime[:,np.newaxis],self.data[:,np.newaxis],axis=1)
-        fmtlist=['%.6e','%.6e']
-        filename = asksaveasfilename(title="Select the folder to save the processed data.", initialdir=self.savedir,filetypes=[("Tab sep file","*.log")],initialfile='Measured_IV')
-        if filename:
-            Files_RW().write_header_data(os.path.dirname(filename),os.path.basename(filename),header,data,fmtlist)
-            self.savedir=os.path.dirname(filename)
-            #self.write_to_ini()
-            self.command_elements['save'].config(state=DISABLED)
+
+    def save_data(self,filename):
+        data={}
+        data['#data_table']=append(self.datatime[:,newaxis],self.data[:,newaxis],axis=1)
+        Write_to.data(filename,data)
+
+#    def save_data(self):
+#        header=[]
+#        text='#comment'
+#        header.append(text)
+#        text="#setup"
+#        header.append(text)
+#        text='#data_header'
+#        header.append(text)
+#        text=f'{self.xname}\t{self.variables["quantity"].get()}'
+#        header.append(text)
+#        text=f'{self.units[self.xname]}\t{self.units[self.variables["quantity"].get()]}'
+#        header.append(text)
+#        text='#data_table'
+#        header.append(text)
+#        data=append(self.datatime[:,newaxis],self.data[:,newaxis],axis=1)
+#        fmtlist=['%.6e','%.6e']
+#        filename = asksaveasfilename(title="Select the folder to save the processed data.", initialdir=self.savedir,filetypes=[("log data file","*.log")],initialfile='Measured_IV')
+#        if filename:
+#            Files_RW().write_header_data(os.path.dirname(filename),os.path.basename(filename),header,data,fmtlist)
+#            self.savedir=os.path.dirname(filename)
+#            self.write_to_ini()
+#            self.command_elements['save'].config(state=DISABLED)
     
     def connect(self):
         self.sock=socket.socket()
@@ -258,8 +250,7 @@ class Logger(AppFrame):
             self.update_time_between()
             self.init_plot_data()
             #fix it once you fix figures
-            self.figure.plot.ax.set_ylabel(f"{self.variables['quantity'].get()} ({self.units[self.variables['quantity'].get()]})")
-            self.figure.canvas.draw()
+            self.figure.plot.update_labels(y=f"{self.variables['quantity'].get()} ({self.units[self.variables['quantity'].get()]})")
             self.disable_settings_elements()
             self.sock.send("*RST\n".encode('utf-8'))
             if "MODEL DAQ6510\n04480963"==self.instname.get_var():
@@ -298,13 +289,13 @@ class Logger(AppFrame):
                  
     def update_min_max(self):
         #for the first run
-        if np.shape(self.data)==np.shape(self.new_data):
-            self.datamin=min(0.98*np.min(self.new_data),1.02*np.min(self.new_data))
-            self.datamax=max(0.98*np.max(self.new_data),1.02*np.max(self.new_data))
+        if shape(self.data)==shape(self.new_data):
+            self.datamin=npmin(0.98*npmin(self.new_data),1.02*npmin(self.new_data))
+            self.datamax=npmax(0.98*npmax(self.new_data),1.02*npmax(self.new_data))
         #for every other
         else:
-            self.datamin=min(self.datamin,0.98*np.min(self.new_data),1.02*np.min(self.new_data))
-            self.datamax=max(self.datamax,0.98*np.max(self.new_data),1.02*np.max(self.new_data))
+            self.datamin=npmin(self.datamin,0.98*npmin(self.new_data),1.02*npmin(self.new_data))
+            self.datamax=npmax(self.datamax,0.98*npmax(self.new_data),1.02*npmax(self.new_data))
             
     
     def collect_plot(self):
@@ -342,7 +333,7 @@ class Logger(AppFrame):
                         self.sock.send("FETC?\nINIT\n".encode())
                         self.get_all_data_agilent(data_points)
                     
-                    self.data=np.append(self.data,self.new_data)
+                    self.data=append(self.data,self.new_data)
                     self.update_plot()
                 self.frameroot.after(int(self.variables['samples'].get()*self.time_between_points*1000*0.7),self.collect_plot)
                 
@@ -358,19 +349,19 @@ class Logger(AppFrame):
                 data.append(item)
             else:
                 times.append(item)
-        self.new_data=np.array(data).astype('float')
-        timearray=np.array(times).astype('float')
+        self.new_data=array(data).astype('float')
+        timearray=array(times).astype('float')
         timearray=timearray-timearray[-1]
-        self.datatime=np.append(self.datatime,timearray+time.time()-self.starttime)
+        self.datatime=append(self.datatime,timearray+time.time()-self.starttime)
     
     
     def get_all_data_agilent(self,data_points):
         data=''        
         while len(data.strip().split(','))!=data_points or data=='':
             data=data+self.sock.recv(2048).decode('utf-8')
-        self.new_data=np.array(data.strip().split(',')).astype(float)
+        self.new_data=array(data.strip().split(',')).astype(float)
         timearray=self.sample_time[len(self.sample_time)-data_points:]
-        self.datatime=np.append(self.datatime,timearray+time.time()-self.starttime)
+        self.datatime=append(self.datatime,timearray+time.time()-self.starttime)
         
         
 
@@ -388,30 +379,30 @@ class Logger(AppFrame):
             else:
                 self.sock.send("FETC?\n".encode())
                 self.get_all_data_agilent(data_points)      
-            self.data=np.append(self.data,self.new_data)
+            self.data=append(self.data,self.new_data)
             self.update_plot()
         self.measurement_init=False;
         self.command_elements['collect'].change_state('off')
-        self.command_elements['save'].configure(state=NORMAL)
+        self.command_elements['save'].config(state=NORMAL)
         self.enable_settings_elements()
         
         
         
     def init_plot_data(self):
-        self.data=np.array([])
-        self.datatime=np.array([]) 
+        self.data=array([])
+        self.datatime=array([]) 
         self.figure.plot.ax.plot(self.datatime,self.data)
     
     def clear_plot_data(self):
         self.starttime=time.time()
-        self.data=np.array([])
-        self.datatime=np.array([])
+        self.data=array([])
+        self.datatime=array([])
         if len(self.figure.plot.ax.lines)!=0:
             self.figure.plot.ax.lines[0].set_xdata(self.datatime)
             self.figure.plot.ax.lines[0].set_ydata(self.data)
         self.figure.plot.ax.set_xlim(0,1)
         self.figure.canvas.draw()
-        self.command_elements['save'].configure(state=DISABLED)
+        self.command_elements['save'].config(state=DISABLED)
     
     def update_plot(self):
         if len(self.figure.plot.ax.lines)!=0:
